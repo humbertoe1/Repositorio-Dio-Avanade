90e8555b81aa237fcec17a5ac1f42234391aa50a
humbertoe1 <humbertoe11@hotmail.com>
2022-01-21 10:32:41 -0300

    Pequeno guia de pilhas adicionado

---
+++ AdvancedColectionsDois/AdvancedColectionsDois/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedColectionsDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A pilha obedece a regra LIFO, o último a entrar é o primeiro a sair
+            Stack<string> pilhaLivros = new Stack<string>();
+
+            //push insere objeto no topo || pop remove objetos || count retorna o tamanho da fila
+
+            
+            pilhaLivros.Push("O senhor dos anéis: O Retorno do Rei");
+            pilhaLivros.Push("O senhor dos anéis: A Sociedade do Anel");
+            pilhaLivros.Push("O senhor dos anéis: As Duas Torres");
+
+            Console.WriteLine($"Quantos livros que faltam ler: {pilhaLivros.Count}");
+            while (pilhaLivros.Count > 0)
+            {
+                Console.WriteLine($"Próximo Livro para leitura é: {pilhaLivros.Peek()}");
+                Console.WriteLine($"{pilhaLivros.Pop()} foi lido com sucesso!");
+            }
+
+            Console.WriteLine($"Quantos livros que faltam ler: {pilhaLivros.Count}");
+        }
+    }
+}
