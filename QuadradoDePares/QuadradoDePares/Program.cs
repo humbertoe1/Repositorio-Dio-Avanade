2dfeefce8e6aca58c40090b63f9f3c1f8394bdd4
humberto <humbertoe11@hotmail.com>
2022-01-22 11:30:37 -0300

    Desafio Quadrado de Pares

---
+++ QuadradoDePares/QuadradoDePares/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace QuadradoDePares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i <= n; i += 2)
+            {
+                if(i == 0)
+                {
+                    continue;
+                }
+                
+                int b = i * i;
+                Console.WriteLine($"{i}^2 = {b}");
+            }
+
+        }
+    }
+}
