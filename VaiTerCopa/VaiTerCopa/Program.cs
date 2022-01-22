d341830643e6b30fde91b6e6f20eb3bea2b02fa7
humberto <humbertoe11@hotmail.com>
2022-01-22 11:58:17 -0300

    Desafio 'Vai ter copa?'

---
+++ VaiTerCopa/VaiTerCopa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VaiTerCopa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroDeReclamacoes;
+            
+            while((numeroDeReclamacoes = Console.ReadLine()) != null)
+            {
+                if (numeroDeReclamacoes == "0")
+                {
+                    Console.WriteLine("vai ter copa!");
+                }
+                else
+                {
+                    Console.WriteLine("vai ter duas!");
+                }
+            }
+        }
+    }
+}
