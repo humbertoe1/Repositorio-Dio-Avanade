cfad6c91ecb6f83f5c1adc85d42a76ff1693a3c0
humberto <humbertoe11@hotmail.com>
2022-01-23 00:02:23 -0300

    Desafio para encontrar o sucessor par

---
+++ SucessorPar/SucessorPar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SucessorPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            if (X % 2 == 0)
+                X+=2;
+            else
+                X++;
+
+            Console.WriteLine($"{X}");
+        }
+    }
+}
